1a161b45b6ccafac77d91975762fdc13fcc8ab00
isanayara <120230311+isanayara@users.noreply.github.com>
2024-10-07 15:41:23 +0000

    aula 07/10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import Footer from "./component/footer";
-
-function Index() {
-  return 
-  <>
-    <NavBar/>
-    <h1>Olá Mundo</h1>
-    <Footer/>
-  </>
-  
-}
-export default Index;
+import Navbar from "./component/navbar";
+import {Container, Row, Col} from 'react-bootstrap';
+export default function Index() {
+  return <>
+    <Navbar />
+    <Container>
+      <Row>
+        <Col className="bg-primary">1 of 2</Col>
+        <Col className="bg-warning">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className="bg-info">1 of 3</Col>
+        <Col className="bg-warning-subtle">2 of 3</Col>
+        <Col className="grad">3 of 3</Col>
+      </Row>
+    </Container>
+    <Footer />
+  </>;
+  }
